225f2135d7842d54bea52a8d5c26acb201b826f1
Jacob Jäger <mail@jljaeger.de>
2018-12-30 02:55:16 +0100

    Added entry_point

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ setup(
     description="Display pictures in a terminal using x256 color codes",
     py_modules=['x256_img'],
     install_requires=["x256", "pillow"],
+    entry_points={
+        'console_scripts': [
+            "x256-img=x256_img:_cli_init"
+        ]
+    },
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
