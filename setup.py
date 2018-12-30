b1cd15530bb074d982876895d00294cd9e5d8601
Jacob Jäger <mail@jljaeger.de>
2018-12-30 02:40:39 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="x256_img",
+    author="Jacob Jäger",
+    maintainer="Jacob Jäger",
+    version="0.0.1",
+    description="Display pictures in a terminal using x256 color codes",
+    py_modules=['metasyntax'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+    ]
+)
